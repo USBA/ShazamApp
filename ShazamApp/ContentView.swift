6d8fc06f65531392a832d58b3b10002408e6428c
Umayanga Alahakoon <umayanga@usa.com>
2021-06-16 18:37:58 +0530

    Initial Commit

---
+++ ShazamApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShazamApp
+//
+//  Created by Umayanga Alahakoon on 6/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShazamApp/ShazamAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShazamAppApp.swift
+//  ShazamApp
+//
+//  Created by Umayanga Alahakoon on 6/16/21.
+//
+
+import SwiftUI
+
+@main
+struct ShazamAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
